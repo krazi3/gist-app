942b7b03b4034e5c874c29af101d47dca4d6f07a
krazi3 <awwabhaq@gmail.com>
2018-04-19 06:20:53 +0500

    Added form to search github gist by username

--- client/components/main.jsx
+++ client/components/main.jsx
@@ -3,12 +3,20 @@ import React, { Component } from 'react';
 export default class Main extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      username: '',
+    }
   }
 
   render() {
     return (
       <div className="container">
         <h1>Welcome</h1>
+        <form>
+          <label>Enter Github Username</label>
+          <input name="username" type="text" value={this.state.username} onChange={event => this.setState({username: event.target.value})}/>
+          <button>Search</button>
+        </form>
       </div>
     )
   }
