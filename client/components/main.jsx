f519735650af4fb21d23d72a3ee464f3c2dcc77f
krazi3 <awwabhaq@gmail.com>
2018-04-19 06:29:43 +0500

    Added pagination on show gist list

--- client/components/main.jsx
+++ client/components/main.jsx
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+function getPageLinks (link) {
+  link = link.link || ''
+
+  const links = {}
+
+  // link format:
+  // '<https://api.github.com/users/aseemk/followers?page=2>; rel="next", <https://api.github.com/users/aseemk/followers?page=2>; rel="last"'
+  link.replace(/<([^>]*)>;\s*rel="([\w]*)"/g, (m, uri, type) => {
+    links[type] = uri
+  })
+
+  return links
+}
+
 export default class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       username: '',
       gists: [],
+      navigation: {},
     }
   }
 
-  search(event) {
+  search(event, url) {
     event.preventDefault();
-    axios.get(`https://api.github.com/users/${this.state.username}/gists`)
+    axios.get(url || `https://api.github.com/users/${this.state.username}/gists`)
       .then(response => {
         this.setState({
           gists: response.data,
+          navigation: getPageLinks(response.headers),
         })
       })
   }
@@ -24,11 +40,13 @@ export default class Main extends Component {
     return (
       <div className="container">
         <h1>Welcome</h1>
-        <form onSubmit={event => this.search(event)}>
+        <form onSubmit={event => this.search(event, '')}>
           <label>Enter Github Username</label>
           <input name="username" type="text" value={this.state.username} onChange={event => this.setState({username: event.target.value})}/>
           <button>Search</button>
           {this.state.gists.map(gist => <p>{Object.keys(gist.files)[0]}</p>)}
+          {this.state.navigation.prev ? <button type="button" onClick={event => this.search(event, this.state.navigation.prev)}>Prev</button> : null}
+          {this.state.navigation.next ? <button type="button" onClick={event => this.search(event, this.state.navigation.next)}>Next</button> : null}
         </form>
       </div>
     )
