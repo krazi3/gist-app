daf6854a5b877614d9e4b8665059c08ac7ccb3a0
krazi3 <awwabhaq@gmail.com>
2018-04-19 06:54:27 +0500

    getPageLinks moved to utils

--- client/components/main.jsx
+++ client/components/main.jsx
@@ -1,20 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Item from './item.jsx';
-
-function getPageLinks (link) {
-  link = link.link || ''
-
-  const links = {}
-
-  // link format:
-  // '<https://api.github.com/users/aseemk/followers?page=2>; rel="next", <https://api.github.com/users/aseemk/followers?page=2>; rel="last"'
-  link.replace(/<([^>]*)>;\s*rel="([\w]*)"/g, (m, uri, type) => {
-    links[type] = uri
-  })
-
-  return links
-}
+import { getPageLinks } from '../utils';
 
 export default class Main extends Component {
   constructor(props) {
---
+++ client/utils.js
@@ -0,0 +1,14 @@
+
+export function getPageLinks (link) {
+  link = link.link || ''
+
+  const links = {}
+
+  // link format:
+  // '<https://api.github.com/users/aseemk/followers?page=2>; rel="next", <https://api.github.com/users/aseemk/followers?page=2>; rel="last"'
+  link.replace(/<([^>]*)>;\s*rel="([\w]*)"/g, (m, uri, type) => {
+    links[type] = uri
+  })
+
+  return links
+}
