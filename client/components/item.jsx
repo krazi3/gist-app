9421c4b70a3a29cb7a4af3cbec06e39441f26f02
krazi3 <awwabhaq@gmail.com>
2018-04-19 07:28:08 +0500

    Fetched list of forks and displayed recent three

--- client/components/item.jsx
+++ client/components/item.jsx
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { getPageLinks } from '../utils';
 
 export default class Item extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      forks: [],
+    }
+  }
+
+  componentDidMount() {
+    let { gist } = this.props;
+    axios.get(gist.forks_url)
+      .then(response => {
+        if(response.headers && response.headers.link) {
+          let links = getPageLinks(response.headers);
+          return axios.get(links.last)
+        }
+        return response;
+      })
+      .then(response => {
+        if(response.data && response.data.length) {
+          this.setState({
+            forks: response.data.reverse().slice(0, 3)
+          })
+        }
+      })
   }
 
   renderTags(files) {
@@ -19,7 +43,12 @@ export default class Item extends Component {
   render() {
     let { gist } = this.props;
     return (
-      <p>{Object.keys(gist.files)[0]} {this.renderTags(gist.files)}</p>
+      <div>
+        <p>{Object.keys(gist.files)[0]} {this.renderTags(gist.files)}</p>
+        <ul>
+          {this.state.forks.map(fork => <li>{fork.owner.login}</li>)}
+        </ul>
+      </div>
     )
   }
 }
