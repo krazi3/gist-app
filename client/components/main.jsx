8de6f43419d4ab3248430456b43f395cab657ad1
krazi3 <awwabhaq@gmail.com>
2018-04-19 06:24:58 +0500

    Gits search api linked

--- client/components/main.jsx
+++ client/components/main.jsx
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       username: '',
+      gists: [],
     }
+  }
+
+  search(event) {
+    event.preventDefault();
+    axios.get(`https://api.github.com/users/${this.state.username}/gists`)
+      .then(response => {
+        this.setState({
+          gists: response.data,
+        })
+      })
   }
 
   render() {
     return (
       <div className="container">
         <h1>Welcome</h1>
-        <form>
+        <form onSubmit={event => this.search(event)}>
           <label>Enter Github Username</label>
           <input name="username" type="text" value={this.state.username} onChange={event => this.setState({username: event.target.value})}/>
           <button>Search</button>
+          {this.state.gists.map(gist => <p>{Object.keys(gist.files)[0]}</p>)}
         </form>
       </div>
     )
